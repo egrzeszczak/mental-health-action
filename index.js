c68429860c16f1767dbdfedf64b6d00ad5b85a03
egrzeszczak <ernest.grzeszczak@protonmail.com>
2022-07-11 21:15:55 +0200

    fix: added semicolons

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const recursive = require("recursive-readdir");
 
-const content = core.getInput('root_dir', { default: ''})
+const content = core.getInput('root_dir', { default: ''});
 
 (
     async () => {
         try {
-            core.notice("Action!")
+            core.notice("Action!");
             await recursive(content, ['!*.php*']).then(
                 files => {
                     for(i = 0; i < files.length; i+=1) {
-                        core.info(`file ${files[i]} found`)
+                        core.info(`file ${files[i]} found`);
                     }
                 }
-            )
+            );
         } catch (error) {
-            core.setFailed(error.message)
+            core.setFailed(error.message);
         }
     }
 )();
