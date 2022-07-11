a77bd79bbaa3f916deddfd689f1585a4f5abfb14
egrzeszczak <ernest.grzeszczak@protonmail.com>
2022-07-11 21:03:32 +0200

    feat: added recursive-readdir, looks for .php files in repo

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const recursive = require("recursive-readdir");
+
+const content = core.getInput('root_dir', { default: ''})
 
 (
     async () => {
         try {
             core.notice("Action!")
+            await recursive(content, ['!*.php*']).then(
+                files => {
+                    for(i = 0; i < files.length; i+=1) {
+                        core.info(`file ${files[i]} found`)
+                    }
+                }
+            )
         } catch (error) {
             core.setFailed(error.message)
         }
