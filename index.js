97880fccfc2f1a9bf537d9abe20e817ed7ac3163
egrzeszczak <ernest.grzeszczak@protonmail.com>
2022-07-11 20:11:38 +0200

    init: boilerplate action.yml

---
+++ index.js
@@ -0,0 +1,12 @@
+const core = require("@actions/core")
+const github = require("@actions/github")
+
+(
+    async () => {
+        try {
+            core.notice("Action!")
+        } catch (error) {
+            core.setFailed(error.message)
+        }
+    }
+)();
